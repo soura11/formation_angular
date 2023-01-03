c42e6b49764ce7f065cf011f701693470f24a39b
soura11 <soya-du-13@hotmail.fr>
2023-01-03 23:51:08 +0100

    home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  etudiants: number =332;
+  dir:any = {prenom: 'Adrien'};
+
+  user : User = {id : 1, name: 'John', email: 'john@yahoo.fr'}
+  
+  afficheMessage(message: string): void {
+    alert("mon message: " + message);
+  }
+
+  afficheMessage2(message: string, event:any): void {
+    alert("mon message: " + message);
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  quantites: number[] = [12, 189, 17, 12];
+
+  users: User[] = [
+    {id : 5, name: 'John', email: 'john@yahoo.fr'},
+    {id : 6, name: 'momo', email: 'momo@yahoo.fr'},
+    {id : 7, name: 'salma', email: 'salma@yahoo.fr'},
+    {id : 8, name: 'lola', email: 'lola12@yahoo.fr'}
+  ];
+
+  data: any[] = [{
+    "id": 1,
+    "name": "abc",
+    "address": {
+      "streetName": "cde",
+      "streetId": 2
+    },
+    "projets": [
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      },
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      }
+  ]
+  },
+  {
+    "id": 2,
+    "name": "marc",
+    "address": {
+      "streetName": "4 rue paradis",
+      "streetId": 13006
+    },
+    "projets": [
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      },
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      }
+  ]
+  }]
+
+  position : number = 2;
+
+}
